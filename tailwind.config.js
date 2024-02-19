7d8feafae9616b3e272c8346eac7776eab7d6524
DhahiraThesneem <dhahirathesneem@gmail.com>
2024-02-19 13:46:08 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      'font-color-white': '#ffffff', //hsl(0, 0%, 100%);
+      'font-color-green': '#c5f82a', //hsl(75, 94%, 57%);
+      'button-color': '#333333', //hsl(0, 0%, 20%);
+      'card-color': '#1f1f1f', //hsl(0, 0%, 12%);
+      'gray-color': '#141414', //hsl(0, 0%, 8%);
+    },
+  },
+  plugins: [],
+}
+
